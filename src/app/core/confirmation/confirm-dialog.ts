2e4cc67531886e91f4482946ce5f3b97fa789ae4
Olivier Dantas Lima <olivier.dantaslima@gmail.com>
2026-05-11 11:18:01 +0200

    feat(confirmation): separate confirm dialog template into HTML file

--- src/app/core/confirmation/confirm-dialog.ts
+++ src/app/core/confirmation/confirm-dialog.ts
@@ -1,7 +1,7 @@
 import { DIALOG_DATA, DialogRef } from "@angular/cdk/dialog";
 import { Component, inject } from "@angular/core";
 
-@Component({ template: '{{title}}<button (click)="dialogRef.close(false)">Annuler</button><button (click)="dialogRef.close(true)">Confirmer</button>' })
+@Component({ templateUrl: './confirm-dialog.html' })
 export class ConfirmDialogComponent {
     readonly title = inject<{ title: string }>(DIALOG_DATA).title;
     readonly dialogRef: DialogRef<boolean> = inject(DialogRef);

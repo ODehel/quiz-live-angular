eccaf13b91019f684215a6df05e5ec0e6fd483fd
Olivier Dantas Lima <olivier.dantaslima@gmail.com>
2026-05-11 11:06:30 +0200

    feat(confirm-dialog): render title and confirm/cancel buttons

--- src/app/core/confirmation/confirm-dialog.spec.ts
+++ src/app/core/confirmation/confirm-dialog.spec.ts
@@ -1,48 +1,67 @@
-import { DIALOG_DATA } from "@angular/cdk/dialog";
-import { TestBed } from "@angular/core/testing";
+import { DIALOG_DATA, DialogRef } from "@angular/cdk/dialog";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ConfirmDialogComponent } from "./confirm-dialog";
+import { Mock } from "vitest";
 
 describe("Confirm Dialog", () => {
     it("renders the title 'Titre'", async () => {
         const title = "Titre";
-        const fixture = await createConfirmComponent(title);
+        const { fixture } = await createConfirmComponent(title);
 
         const text = (fixture.nativeElement as HTMLElement).textContent;
         expect(text).toContain(title);
     });
     it("renders the title 'Question'", async () => {
         const title = "Question";
-        const fixture = await createConfirmComponent(title);
+        const { fixture } = await createConfirmComponent(title);
 
         const text = (fixture.nativeElement as HTMLElement).textContent;
         expect(text).toContain(title);
     });
     it("renders the 'Confirmer' button", async () => {
-        const fixture = await createConfirmComponent("Confirmez-vous ?");
+        const { fixture } = await createConfirmComponent("Confirmez-vous ?");
 
-        const root = fixture.nativeElement as HTMLElement;
-        const buttons = Array.from(root.querySelectorAll<HTMLButtonElement>('button'));
-        const confirmButton = buttons.find(b => b.textContent === 'Confirmer');
-
+        const confirmButton = getButton(fixture, "Confirmer");
         expect(confirmButton).not.toBeUndefined();
     });
     it("renders the 'Annuler' button", async () => {
-        const fixture = await createConfirmComponent("Confirmez-vous ?");
+        const { fixture } = await createConfirmComponent("Confirmez-vous ?");
 
-        const root = fixture.nativeElement as HTMLElement;
-        const buttons = Array.from(root.querySelectorAll<HTMLButtonElement>('button'));
-        const cancelButton = buttons.find(b => b.textContent === 'Annuler');
-
+        const cancelButton = getButton(fixture, "Annuler");
         expect(cancelButton).not.toBeUndefined();
     });
+    it("calls DialogRef.close(true) when clicking on 'Confirmer' button", async () => {
+        const { fixture, fakeDialogRef } = await createConfirmComponent("Confirmez-vous ?");
+
+        const confirmButton = getButton(fixture, "Confirmer");
+        confirmButton?.click();
+        expect(fakeDialogRef.close).toHaveBeenCalledWith(true);
+
+    });
+    it("calls DialogRef.close(false) when clicking on 'Annuler' button", async () => {
+        const { fixture, fakeDialogRef } = await createConfirmComponent("Confirmez-vous ?");
+
+        const cancelButton = getButton(fixture, "Annuler");
+        cancelButton?.click();
+        expect(fakeDialogRef.close).toHaveBeenCalledWith(false);
+    });
     async function createConfirmComponent(title: string) {
+        let fakeDialogRef: { close: Mock };
+        fakeDialogRef = { close: vi.fn() }
         await TestBed.configureTestingModule({
             providers: [
-                { provide: DIALOG_DATA, useValue: { title } }
+                { provide: DIALOG_DATA, useValue: { title } },
+                { provide: DialogRef, useValue: fakeDialogRef }
             ]
         }).compileComponents();
         const fixture = TestBed.createComponent(ConfirmDialogComponent);
         await fixture.whenStable();
-        return fixture;
+        return { fixture, fakeDialogRef };
+    }
+
+    function getButton(fixture: ComponentFixture<ConfirmDialogComponent>, buttonContent: string) {
+        const root = fixture.nativeElement as HTMLElement;
+        const buttons = Array.from(root.querySelectorAll<HTMLButtonElement>('button'));
+        return buttons.find(b => b.textContent === buttonContent);
     }
 });
--- src/app/core/confirmation/confirm-dialog.ts
+++ src/app/core/confirmation/confirm-dialog.ts
@@ -1,7 +1,8 @@
-import { DIALOG_DATA } from "@angular/cdk/dialog";
+import { DIALOG_DATA, DialogRef } from "@angular/cdk/dialog";
 import { Component, inject } from "@angular/core";
 
-@Component({ template: '{{title}}<button>Annuler</button><button>Confirmer</button>' })
+@Component({ template: '{{title}}<button (click)="dialogRef.close(false)">Annuler</button><button (click)="dialogRef.close(true)">Confirmer</button>' })
 export class ConfirmDialogComponent {
     readonly title = inject<{ title: string }>(DIALOG_DATA).title;
+    readonly dialogRef: DialogRef<boolean> = inject(DialogRef);
 }

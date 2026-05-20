dbc4f6a1fdb1b9664dd117956b6c0564f09325b6
Olivier Dantas Lima <olivier.dantaslima@gmail.com>
2026-05-20 15:49:28 +0200

    feat(playground): add Playground component and configure routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Playground } from './features/playground/playground';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'playground', component: Playground }
+];
---
+++ src/app/features/playground/playground.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-playground',
+    template: 'The playground works!!!'
+})
+export class Playground {
+
+}

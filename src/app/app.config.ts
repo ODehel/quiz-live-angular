0a21b8085c637a5cb65b57030ab3c779983ae8b5
Olivier Dantas Lima <olivier.dantaslima@gmail.com>
2026-07-05 15:52:54 +0200

    feat(auth): acquire the token at startup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom, provideAppInitializer, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { CircleAlert, CirclePlay, LogOut, LucideAngularModule, Play, Trash2, TriangleAlert, X, Check, ArrowLeft, WifiOff, ServerCrash, MapPinOff, OctagonAlert, Zap, ShieldX } from 'lucide-angular';
 import { routes } from './app.routes';
+import { AuthService } from './core/authentication/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,6 +11,10 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(LucideAngularModule.pick({
       X, TriangleAlert, CircleAlert, CirclePlay, Play, Trash2, LogOut, Check, ArrowLeft, WifiOff, ServerCrash, MapPinOff, OctagonAlert,
       Zap, ShieldX
-    }))
+    })),
+    provideAppInitializer(() => {
+      const authService = inject(AuthService);
+      authService.initialize();
+    })
   ]
 };
